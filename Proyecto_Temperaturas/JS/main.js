f8a74d6d3fee50db7950dd981a2f2a72e1d74d7e
sebas2516 <110509771+sebas2516@users.noreply.github.com>
2023-01-13 10:00:43 -0500

    Add files via upload

--- Proyecto_Temperaturas/JS/main.js
+++ Proyecto_Temperaturas/JS/main.js
@@ -14,6 +14,7 @@
 //  })
 // })
 
+// Sign-In
 const signinForm = document.querySelector('#signup-form');
 
 signinForm.addEventListener('submit', (e)=>{
@@ -22,17 +23,36 @@ signinForm.addEventListener('submit', (e)=>{
     const email = document.querySelector('#email').value;
     const pass = document.querySelector('#password').value;
 
-    
     auth
+
     .signInWithEmailAndPassword(email,pass)
     
     .then(userCredential =>{
-        
         signinForm.reset();
 
         location.replace("vehiculos.html")
 
         console.log('sign in')
+    })
+
+    .catch((error) => {
+        alert("Usuario o contraseña no validos")
     });
 });
 
+// View Password
+let password = document.getElementById('password');
+let viewPassword = document.getElementById('view');
+let click = false;
+
+viewPassword.addEventListener('click', (e)=>{
+    if(!click){
+        password.type = 'text'  
+        document.getElementById('view1').className = 'bi bi-eye-fill'
+        click = true
+    }else if(click){
+        password.type = 'password'
+        document.getElementById('view1').className = 'bi bi-eye-slash-fill'
+        click = false
+    }
+});
